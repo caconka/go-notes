02619f6e5ad86b0f12048e3d030e6494b176d0b5
Carlos Cabezas <carloscabezascontra@gmail.com>
2018-10-01 17:31:38 +0200

    interfaces

---
+++ interfaces/interfaces.go
@@ -0,0 +1,107 @@
+package main
+
+import "fmt"
+
+type SalaryCalculator interface {
+	CalculateSalary() int
+}
+
+type Permanent struct {
+	empId    int
+	basicpay int
+	pf       int
+}
+
+type Contract struct {
+	empId    int
+	basicpay int
+}
+
+// Tanto la estructura Permanent como Contract implementan la interfaz
+// SalaryCalculator, y cada uno decide cómo implementar los métodos
+func (p Permanent) CalculateSalary() int {
+	return p.basicpay + p.pf
+}
+
+func (c Contract) CalculateSalary() int {
+	return c.basicpay
+}
+
+func totalExpense(s []SalaryCalculator) {
+	expense := 0
+	for _, v := range s {
+		expense = expense + v.CalculateSalary()
+	}
+	fmt.Printf("Total Expense Per Month $%d \n", expense)
+}
+
+/* --------------------------------------- */
+func findType(i interface{}) {
+	switch i.(type) {
+	case string:
+		fmt.Printf("̀̀I am a string and my value is %s\n", i.(string))
+	case int:
+		fmt.Printf("I am an int and my value is %d\n", i.(int))
+	default:
+		fmt.Printf("Unknown type\n")
+	}
+}
+
+/* --------------------------------------- */
+type SalaryCalculator2 interface {
+	DisplaySalary()
+}
+
+type LeaveCalculator interface {
+	CalculateLeavesLeft() int
+}
+
+// Embedding interfaces, se puede hacer de esta manera o implementar varias por
+// separado
+type EmployeeOperations interface {
+	SalaryCalculator2
+	LeaveCalculator
+}
+
+type Employee struct {
+	firstName   string
+	lastName    string
+	basicPay    int
+	pf          int
+	totalLeaves int
+	leavesTaken int
+}
+
+func (e Employee) DisplaySalary() {
+	fmt.Printf("%s %s has salary $%d", e.firstName, e.lastName, (e.basicPay + e.pf))
+}
+
+func (e Employee) CalculateLeavesLeft() int {
+	return e.totalLeaves - e.leavesTaken
+}
+
+func main() {
+	pemp1 := Permanent{1, 5000, 20}
+	pemp2 := Permanent{2, 6000, 30}
+	cemp1 := Contract{3, 3000}
+	employees := []SalaryCalculator{pemp1, pemp2, cemp1}
+	totalExpense(employees)
+
+	/* --------------------------------------- */
+	findType("Naveen")
+	findType(77)
+	findType(89.98)
+
+	/* --------------------------------------- */
+	e := Employee{
+		firstName:   "Naveen",
+		lastName:    "Ramanathan",
+		basicPay:    5000,
+		pf:          200,
+		totalLeaves: 30,
+		leavesTaken: 5,
+	}
+	var empOp EmployeeOperations = e
+	empOp.DisplaySalary()
+	fmt.Println("\nLeaves left =", empOp.CalculateLeavesLeft())
+}
