54a2c57086100048da2e5234b85c2e6d4ca6c99e
Carlos Cabezas <carloscabezascontra@gmail.com>
2018-10-01 09:12:31 +0200

    functions

---
+++ functions.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+/* func <funcName> (paramName type) returnType ́{...} */
+
+// Como <area> y <perimeter> están declarados como valores de retorno, no es
+// necesario especificarlos en el return
+func rectProps(length, width float64) (area, perimeter float64) {
+	area = length * width
+	perimeter = (length + width) * 2
+	return
+}
+
+func main() {
+	// _ Blank identifier: desecha un valor retornado, en este caso es <perimeter>
+	area, _ := rectProps(10.8, 5.6)
+	fmt.Printf("Area %f", area)
+}
